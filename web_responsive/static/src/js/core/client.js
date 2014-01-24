7365fca24be784d1b6479373595f0510d6868eb1
Chien Nguyen Minh <chien@trobz.com>
2014-01-24 18:34:48 +0700

    5363: Depending on the login device, automatically select mobile / desktop version

--- web_responsive/static/src/js/core/client.js
+++ web_responsive/static/src/js/core/client.js
@@ -9,6 +9,21 @@ openerp.unleashed.module('web_responsive').ready(function(instance){
             var ret = this._super.apply(this, arguments);
             this.menu.on('user_logout', this, this.on_logout);
             return ret;
-        }
+        }, 
+        
+		on_logout: function() {
+	        var self = this;
+	        if (!this.has_uncommitted_changes()) {
+	        	
+	            // Delete force_mobile cookie
+	            this.session.set_cookie('force_mobile', '');
+	            
+	            this.session.session_logout().done(function () {
+	                $(window).unbind('hashchange', self.on_hashchange);
+	                self.do_push_state({});
+	                window.location.reload();
+	            });
+	        }
+	    },
     });
 });
